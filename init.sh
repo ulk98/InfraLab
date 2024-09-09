9cd1a5a3e309ad42e29c4c77a338342b81161c38
ulk98 <ulrichnoukeu98@gmail.com>
2024-09-09 21:18:09 +0000

    modification de init.sh

--- init.sh
+++ init.sh
@@ -13,3 +13,9 @@ sudo systemctl enable docker
 #Création du réseau docker pour les conteneurs critiques de l'infrastructure
 docker network create --driver bridge --subnet=172.40.0.0/24 infra_net
 
+#Construction des images personnalisées docker
+docker build -t test/gateway ./Lab/gateway/
+
+docker build -t test/vuln-ubuntu ./Lab/cible/
+
+docker build -t test/kali_attack ./Lab/attaquant/
